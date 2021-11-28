95e096ff608bec3feca2b094a34d14297065333c
wangjie <wangjie_fourth@163.com>
2021-11-28 23:15:48 +0800

    add: 测试用例

--- capa-bff-api-http/src/test/java/group/rxcloud/capa/bff/api/http/integration/SpringBootCapaApiApplicationTests.java
+++ capa-bff-api-http/src/test/java/group/rxcloud/capa/bff/api/http/integration/SpringBootCapaApiApplicationTests.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 public class SpringBootCapaApiApplicationTests {
@@ -17,10 +19,20 @@ public class SpringBootCapaApiApplicationTests {
     @Resource
     private CapaApiHttpController capaApiHttpController;
 
+    private static List<String> requestStrList = new ArrayList<>();
 
-    @Test
-    public void test() throws Exception {
+    private static List<String> responseStrList = new ArrayList<>();
 
+    static {
+        String responseStr = "{\"body\":{\"id\":1},\"errors\":[]}";
+        responseStrList.add(responseStr);
+        responseStr = "{\"body\":{\"resultInfo\":{\"data\":[{\"name\":\"test1\",\"id\":1},{\"name\":\"test2\",\"id\":2},{\"name\":\"test3\",\"id\":3},{\"name\":\"test4\",\"id\":4},{\"name\":\"test5\",\"id\":5},{\"name\":\"test6\",\"id\":6},{\"name\":\"test7\",\"id\":7},{\"name\":\"test8\",\"id\":8},{\"name\":\"test9\",\"id\":9},{\"name\":\"test10\",\"id\":10}],\"status\":200}},\"errors\":[]}";
+        responseStrList.add(responseStr);
+        responseStr = "{\"body\":{\"resultInfo\":{\"id\":1,\"pet\":{\"name\":\"test1\",\"id\":1}}},\"errors\":[]}";
+        responseStrList.add(responseStr);
+    }
+
+    static {
         String requestParam = "{\n" +
                 "  \"http://127.0.0.1:8089\": [\n" +
                 "    \"/api/v1/pet\":(\n" +
@@ -34,12 +46,51 @@ public class SpringBootCapaApiApplicationTests {
                 "    )\n" +
                 "  ]\n" +
                 "}";
-        String bff = capaApiHttpController.bff(requestParam);
-        JSONObject response = JSON.parseObject(bff);
-        JSONArray obj = (JSONArray) JsonValueMapper.findValueByPointPath(response, "errors");
-        Assertions.assertEquals(0, obj.size());
+        requestStrList.add(requestParam);
+        requestParam = "{\n" +
+                "  \"http://localhost:8089\": [\n" +
+                "    \"/api/v1/list\": (\n" +
+                "      request: {}, \n" +
+                "      response: {\n" +
+                "            \"*\" : \"resultInfo\" \n" +
+                "      }\n" +
+                "    )\n" +
+                "  ]\n" +
+                "}";
+        requestStrList.add(requestParam);
+        requestParam = "{\n" +
+                "  \"http://localhost:8089\": [\n" +
+                "    \"/api/v1/pet\": (\n" +
+                "      request: {}, \n" +
+                "      response: {\n" +
+                "            \"id\" : \"resultInfo.id\" \n" +
+                "      }\n" +
+                "    ),\n" +
+                "    \"/api/v1/id\": (\n" +
+                "      request: {\n" +
+                "          \"id\": \"${resultInfo.id}\"\n" +
+                "      }, \n" +
+                "      response: {\n" +
+                "            \"*\" : \"resultInfo.pet\" \n" +
+                "      }\n" +
+                "    )\n" +
+                "  ]\n" +
+                "}";
+        requestStrList.add(requestParam);
+    }
 
+    @Test
+    public void test() {
 
+        for (int i = 0; i < requestStrList.size(); i++) {
+            String requestParam = requestStrList.get(i);
+            String bff = capaApiHttpController.bff(requestParam);
+            String responseStr = responseStrList.get(i);
+            Assertions.assertEquals(responseStr, bff);
+            JSONObject response = JSON.parseObject(bff);
+            JSONArray obj = (JSONArray) JsonValueMapper.findValueByPointPath(response, "errors");
+            Assertions.assertEquals(0, obj.size());
+        }
 
     }
 }
